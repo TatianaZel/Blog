a6999e117a123ae8aff2ae7e226fb15fb28da176
Tatyana <t.e.zelenskaya@gmail.com>
2017-11-02 12:06:08 +0200

    styles for layout

--- public/build/app.js
+++ public/build/app.js
@@ -372,6 +372,7 @@ app.factory('chatService', ['localStorageService', '$rootScope', '$anchorScroll'
 
 app.factory('notificationService', ['$rootScope', '$timeout', ($rootScope, $timeout) => {
         var notifications = [];
+        var notificationId = 0;
 
         return {
 
--- public/src/services/notifycation.service.js
+++ public/src/services/notifycation.service.js
@@ -1,5 +1,6 @@
 app.factory('notificationService', ['$rootScope', '$timeout', ($rootScope, $timeout) => {
         var notifications = [];
+        var notificationId = 0;
 
         return {
 
